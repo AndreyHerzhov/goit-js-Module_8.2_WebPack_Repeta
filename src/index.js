f813dff493682fca42921324844a626936be11fe
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-23 10:07:15 +0300

    webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import name from './module-1'
+
+console.log('Hallo JS')
+console.log(name)
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const { resolve } = require('path')
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-bundle.js'
+    }
+}
